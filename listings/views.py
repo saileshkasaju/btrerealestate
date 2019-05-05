e938db202687daaed99182e8db0c2c4d25f62c4f
saileshkasaju <leosailesh@gmail.com>
2019-05-05 21:13:34 +0545

    listings made dynamic with pagination

--- listings/views.py
+++ listings/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
+from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 
 from .models import Listing
 
 def index(request):
-  listings = Listing.objects.all()
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+  pagintor = Paginator(listings, 2)
+  page = request.GET.get('page')
+  page_listings = pagintor.get_page(page)
   context = {
-    'listings': listings
+    'listings': page_listings
   }
   return render(request, 'listings/listings.html', context)
 
 def search(request):
   return render(request, 'listings/search.html')
 
-def listing(request):
+def listing(request, listing_id):
   return render(request, 'listings/listing.html')
