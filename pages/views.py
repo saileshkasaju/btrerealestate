92f697fabf0daae895cdb568d3819076ba1da8db
saileshkasaju <leosailesh@gmail.com>
2019-05-06 09:16:11 +0545

    search implementation done

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.choices import price_choices, bedroom_choices, state_choices
 from listings.models import Listing
 from realtors.models import Realtor
 
 def index(request):
   listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
   context = {
-    'listings': listings
+    'listings': listings,
+    'state_choices': state_choices,
+    'bedroom_choices': bedroom_choices,
+    'price_choices': price_choices,
   }
 
   return render(request, 'pages/index.html', context)
@@ -24,3 +28,9 @@ def about(request):
   }
 
   return render(request, 'pages/about.html', context)
+
+
+  # sc 101215137601000017
+  # branch balaju
+  # sc/cid 251.13.022
+  # cid 2746
