2da74c63563a07f228eb8cbab8261026f51c44f6
saileshkasaju <leosailesh@gmail.com>
2019-05-09 12:47:58 +0545

    add local_settings import

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'btrerealestate@gmail.com'
 EMAIL_HOST_PASSWORD = 'Test@1234'
 EMAIL_USE_TLS = True
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
