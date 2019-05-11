a4ddc70b0d0a3942ac425daebb66480398bcb46e
saileshkasaju <leosailesh@gmail.com>
2019-05-11 21:00:37 +0545

    contact inquiry submission api done

--- contacts/views.py
+++ contacts/views.py
@@ -4,36 +4,43 @@ from django.core.mail import send_mail
 from .models import Contact
 
 # Create your views here.
+
+
 def contact(request):
-  if request.method == 'POST':
-    listing_id = request.POST['listing_id']
-    listing = request.POST['listing']
-    name = request.POST['name']
-    email = request.POST['email']
-    phone = request.POST['phone']
-    message = request.POST['message']
-    realtor_email = request.POST['realtor_email']
-    user_id = 0
-    # Check if user has made inquiry already
-    if request.user.is_authenticated:
-      user_id = request.user.id
-      has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
-      if has_contacted:
-        messages.error(request, 'You have already made an inquiry for this listing')
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        realtor_email = request.POST['realtor_email']
+        user_id = 0
+        # Check if user has made inquiry already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(
+                listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(
+                    request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name,
+                          email=email, phone=phone, message=message, user_id=user_id)
+
+        contact.save()
+
+        # Send email
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been and inquiry for ' + listing +
+            '. Sign into the admin panel for more info.',
+            'btrerealestate@gmail.com',
+            [realtor_email, 'saileshworks100@gmail.com'],
+            fail_silently=False
+        )
+
+        messages.success(
+            request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
-    
-    contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
-    
-    contact.save()
-
-    # Send email
-    send_mail(
-      'Property Listing Inquiry',
-      'There has been and inquiry for ' + listing + '. Sign into the admin panel for more info.',
-      'btrerealestate@gmail.com',
-      [realtor_email, 'saileshworks100@gmail.com'],
-      fail_silently=False
-    )
-
-    messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
-    return redirect('/listings/'+listing_id)
